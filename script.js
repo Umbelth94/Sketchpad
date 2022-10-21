d3c21faa5c440b41b4817c642f97449c22190621
Travis Umbel <Umbelth94@gmail.com>
2022-10-20 19:25:11 -0500

    Add default 16x16 grid

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 //Create a button listener that will prompt the user to enter a number
     //Store that number as a variable that will be used to create the dimensions of the grid (16 = 16x16 grid);
 
-createGrid(16); //Start with a default grid size of 16x16
-
-//Creates a grid
-const padContainer = document.getElementById('padcontainer');
-
-let mouseDown = false;
-document.body.onmousedown = () => (mouseDown = true);
-document.body.onmouseup = () => (mouseDown = false); //This is to set up the ability to only draw when mouse is clicked down
-
-function createGrid(dimension){
+    
+    //Creates a grid
+    const padContainer = document.getElementById('padcontainer');
+    
+    let mouseDown = false;
+    document.body.onmousedown = () => (mouseDown = true);
+    document.body.onmouseup = () => (mouseDown = false); //This is to set up the ability to only draw when mouse is clicked down
+    
+    createGrid(16); //Start with a default grid size of 16x16
+    function createGrid(dimension){
     dimension = prompt('How many pixels (squared) would you like your canvas to have?');
     if (dimension > 100){
         alert('You must pick a number that is not higher than 100');
@@ -35,12 +35,12 @@ function drawColor(e) {
         e.target.style.backgroundColor = 'black'; //You can add other options for colors here later
     }}; 
 
-    function deleteGrid(){
-    let child = padContainer.firstElementChild;
-    while (child){
-        padContainer.removeChild(child);
-        child = padContainer.firstElementChild;
-    }};
+function deleteGrid(){
+let child = padContainer.firstElementChild;
+while (child){
+    padContainer.removeChild(child);
+    child = padContainer.firstElementChild;
+}};
 
 const resetButton = document.getElementById('reset');
 resetButton.addEventListener('click', ()=> {
