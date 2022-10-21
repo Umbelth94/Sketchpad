7554875b7a191d2d6016e0a15e1d39b0d2cc24ef
Travis Umbel <Umbelth94@gmail.com>
2022-10-20 19:15:21 -0500

    Add click + drag feature

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 //Create a button listener that will prompt the user to enter a number
     //Store that number as a variable that will be used to create the dimensions of the grid (16 = 16x16 grid);
 
-
+createGrid(16); //Start with a default grid size of 16x16
 
 //Creates a grid
 const padContainer = document.getElementById('padcontainer');
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false); //This is to set up the ability to only draw when mouse is clicked down
+
 function createGrid(dimension){
-    dimension = prompt('How many pixels would you like your canvas to have?');
+    dimension = prompt('How many pixels (squared) would you like your canvas to have?');
     if (dimension > 100){
         alert('You must pick a number that is not higher than 100');
-        dimension = prompt('How many pixels would you like your canvas to have?');
+        dimension = prompt('How many pixels (squared) would you like your canvas to have?');
     }
     for (let i = 0; i< dimension; i++){
-        console.log(`creating row ${i}`)
     let createRow = document.createElement('div');
     createRow.classList.add('row');
     padContainer.appendChild(createRow);
@@ -21,11 +25,17 @@ function createGrid(dimension){
             let createSquare = document.createElement('div');
             createSquare.classList.add('square');
             createRow.appendChild(createSquare);
-            createSquare.addEventListener('mousedown',() => {
-                createSquare.style.backgroundColor='black';
-            })};};};
+            createSquare.addEventListener('mouseover', drawColor); 
+            createSquare.addEventListener('mousedown', drawColor);
+            }};};
+            
+function drawColor(e) {
+    if (e.type ==='mouseover' && !mouseDown) return;
+    if (e.type ==='mouseover' && mouseDown){
+        e.target.style.backgroundColor = 'black'; //You can add other options for colors here later
+    }}; 
 
-function deleteGrid(){
+    function deleteGrid(){
     let child = padContainer.firstElementChild;
     while (child){
         padContainer.removeChild(child);
